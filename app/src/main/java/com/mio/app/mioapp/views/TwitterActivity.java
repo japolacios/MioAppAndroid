6486eb117a8bb20a375842074a88d8171cc9de9e
Japo <japolacios@gmail.com>
2017-11-29 13:37:06 -0500

    Twitter main, No Menu

--- app/src/main/java/com/mio/app/mioapp/views/TwitterActivity.java
+++ app/src/main/java/com/mio/app/mioapp/views/TwitterActivity.java
@@ -2,14 +2,10 @@ package com.mio.app.mioapp.views;
 
 import android.app.ListActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.mio.app.mioapp.R;
-import com.nightonke.boommenu.BoomButtons.HamButton;
-import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomMenuButton;
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
@@ -37,7 +33,7 @@ public class TwitterActivity extends ListActivity {
         setContentView(R.layout.twitter_layout);
         mContext = this;
 
-        bmb = (BoomMenuButton) findViewById(R.id.bmb);
+
 
         TwitterConfig config = new TwitterConfig.Builder(this)
                 .logger(new DefaultLogger(Log.DEBUG))
@@ -61,55 +57,4 @@ public class TwitterActivity extends ListActivity {
     }
 
 
-    private void setBuilders(){
-        HamButton.Builder builder1 = new HamButton.Builder()
-                .normalImageRes(R.drawable.ic_traza_ruta)
-                .normalText("Planear ruta")
-                .listener(new OnBMClickListener() {
-                    @Override
-                    public void onBoomButtonClick(int index) {
-                        // When the boom-button corresponding this builder is clicked.
-                        Toast.makeText(TwitterActivity.this, "Clicked " + index, Toast.LENGTH_SHORT).show();
-                    }
-                });
-        bmb.addBuilder(builder1);
-
-        HamButton.Builder builder2 = new HamButton.Builder()
-                .normalImageRes(R.drawable.ic_real_time)
-                .normalText("Ubicar Rutas")
-                .listener(new OnBMClickListener() {
-                    @Override
-                    public void onBoomButtonClick(int index) {
-                        // When the boom-button corresponding this builder is clicked.
-                        Toast.makeText(TwitterActivity.this, "Clicked " + index, Toast.LENGTH_SHORT).show();
-                    }
-                });
-        bmb.addBuilder(builder2);
-
-        HamButton.Builder builder3 = new HamButton.Builder()
-                .normalImageRes(R.drawable.ic_noticias)
-                .normalText("Noticias")
-                .listener(new OnBMClickListener() {
-                    @Override
-                    public void onBoomButtonClick(int index) {
-                        // When the boom-button corresponding this builder is clicked.
-                        Toast.makeText(TwitterActivity.this, "Clicked " + index, Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(TwitterActivity.this, TwitterActivity.class);
-                        startActivity(intent);
-                    }
-                });
-        bmb.addBuilder(builder3);
-
-        HamButton.Builder builder4 = new HamButton.Builder()
-                .normalImageRes(R.drawable.ic_puntos_recarga)
-                .normalText("Puntos de recarga")
-                .listener(new OnBMClickListener() {
-                    @Override
-                    public void onBoomButtonClick(int index) {
-                        // When the boom-button corresponding this builder is clicked.
-                        Toast.makeText(TwitterActivity.this, "Clicked " + index, Toast.LENGTH_SHORT).show();
-                    }
-                });
-        bmb.addBuilder(builder4);
-    }
 }
